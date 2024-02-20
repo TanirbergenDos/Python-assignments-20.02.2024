d2fb42ed930319df2a4a410e755c1d0c497c0f71
TanirbergenDos <tanirbergendos2006@gmail.com>
2024-02-20 23:40:34 +0600

    Add task_2

---
+++ task_2/task_2.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.message import EmailMessage
+
+# Determine your email
+EMAIL_FROM = "your_email"
+# Define the APP password (better to reference externally)
+PASSWORD = "your_app_password"
+
+
+def send_mail_with_excel(recipient_email, subject, content, excel_file):
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = EMAIL_FROM
+    msg['To'] = recipient_email
+    msg.set_content(content)
+
+    with open(excel_file, 'rb') as f:
+        file_data = f.read()
+    msg.add_attachment(file_data, maintype="application", subtype="xlsx", filename=excel_file)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_FROM, PASSWORD)
+        smtp.send_message(msg)
+
+
+def main():
+    filename = r"..\task_1\мои документы\skcu\Tcspykjsni2024-02-20261.xlsx"
+
+    # Determine recipient's email
+    email_to = "recipient's_email"
+
+    subject = "Excel file from the first task"  
+    send_mail_with_excel(email_to, subject, "CONTENT", filename)
+
+
+if __name__ == "__main__":
+    main()
